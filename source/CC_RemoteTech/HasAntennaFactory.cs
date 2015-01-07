121c0a31178c1cfba96e6dca999a731b34cd659f
Jeremie Rossignol <jrossignol@gmail.com>
2015-01-06 18:58:37 -0700

    HasAntenna

---
+++ source/CC_RemoteTech/HasAntennaFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using ContractConfigurator.Parameters;
+
+namespace ContractConfigurator.RemoteTech
+{
+    /// <summary>
+    /// ParameterFactory wrapper for HasAntennaParameter ContractParameter.
+    /// </summary>
+    public class HasAntennaFactory : ParameterFactory
+    {
+        protected int minCount;
+        protected int maxCount;
+        protected bool activeVessel;
+        protected double minRange;
+        protected double maxRange;
+        protected HasAntennaParameter.AntennaType? antennaType;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<int>(configNode, "minCount", ref minCount, this, 1, x => Validation.GE(x, 0));
+            valid &= ConfigNodeUtil.ParseValue<int>(configNode, "maxCount", ref maxCount, this, int.MaxValue, x => Validation.GE(x, 0));
+            valid &= ConfigNodeUtil.ParseValue<bool>(configNode, "activeVessel", ref activeVessel, this, false);
+            valid &= ConfigNodeUtil.ParseValue<double>(configNode, "minRange", ref minRange, this, 0.0, x => Validation.GE(x, 0.0));
+            valid &= ConfigNodeUtil.ParseValue<double>(configNode, "maxRange", ref maxRange, this, double.MaxValue, x => Validation.GE(x, 0.0));
+            valid &= ConfigNodeUtil.ParseValue<HasAntennaParameter.AntennaType?>(configNode, "antennaType", ref antennaType, this, (HasAntennaParameter.AntennaType?)null);
+            valid &= ConfigNodeUtil.MutuallyExclusive(configNode, new string[] { "activeVessel" }, new string[] { "targetBody" }, this);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            HasAntennaParameter param = new HasAntennaParameter(minCount, maxCount, title);
+            param.activeVessel = activeVessel;
+            param.targetBody = activeVessel ? null : targetBody;
+            param.minRange = minRange;
+            param.maxRange = maxRange;
+            param.antennaType = antennaType;
+
+            return param;
+        }
+    }
+}
---
+++ source/CC_RemoteTech/HasAntennaParameter.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using RemoteTech;
+
+namespace ContractConfigurator.RemoteTech
+{
+    /// <summary>
+    /// Parameter for checking whether the vessel has an antenna that meets the specified criteria.
+    /// </summary>
+    public class HasAntennaParameter : RemoteTechParameter
+    {
+        public enum AntennaType
+        {
+            Dish,
+            Omni
+        };
+
+        protected string title { get; set; }
+        protected int minCount { get; set; }
+        protected int maxCount { get; set; }
+        public CelestialBody targetBody = null;
+        public bool activeVessel = false;
+        public AntennaType? antennaType = null;
+        public double minRange = 0.0;
+        public double maxRange = double.MaxValue;
+
+        public HasAntennaParameter()
+            : this(1)
+        {
+        }
+
+        public HasAntennaParameter(int minCount = 1, int maxCount = int.MaxValue, string title = null)
+            : base()
+        {
+            this.minCount = minCount;
+            this.maxCount = maxCount;
+            this.title = title;
+        }
+
+        protected override string GetTitle()
+        {
+            string output = null;
+            if (string.IsNullOrEmpty(title))
+            {
+                output = (antennaType == null ? "Antenna" : antennaType.ToString()) + ":";
+
+                bool needsComma = false;
+                if (targetBody != null || activeVessel)
+                {
+                    output += needsComma ? "; " : " ";
+                    output += "Target: ";
+                    if (activeVessel)
+                    {
+                        output += "Active Vessel";
+                    }
+                    else
+                    {
+                        output += targetBody.name;
+                    }
+                    needsComma = true;
+                }
+
+                if (minRange != 0.0 || maxRange != double.MaxValue)
+                {
+                    output += needsComma ? "; " : " ";
+                    output += "Range: ";
+                    if (maxRange == double.MaxValue)
+                    {
+                        output += "At least " + RemoteTechAssistant.RangeString(minRange);
+                    }
+                    else if (minRange == 0)
+                    {
+                        output += "At most " + RemoteTechAssistant.RangeString(maxRange);
+                    }
+                    else
+                    {
+                        output += "Between " + RemoteTechAssistant.RangeString(minRange) + " and " + RemoteTechAssistant.RangeString(maxRange);
+                    }
+                    needsComma = true;
+                }
+
+                if (minCount != 0 && maxCount != int.MaxValue)
+                {
+                    output += needsComma ? ": " : " ";
+                    if (maxCount == 0)
+                    {
+                        output += "None";
+                    }
+                    else if (maxCount == int.MaxValue)
+                    {
+                        output += "At least " + minCount;
+                    }
+                    else if (minCount == 0)
+                    {
+                        output += "At most " + maxCount;
+                    }
+                    else if (minCount == maxCount)
+                    {
+                        output += "Exactly " + minCount;
+                    }
+                    else
+                    {
+                        output += "Between " + minCount + " and " + maxCount;
+                    }
+                }
+            }
+            else
+            {
+                output = title;
+            }
+            return output;
+        }
+
+        protected override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+            if (title != null)
+            {
+                node.AddValue("title", title);
+            }
+            node.AddValue("minRange", minRange);
+            if (maxRange != double.MaxValue)
+            {
+                node.AddValue("maxRange", maxRange);
+            }
+            if (targetBody != null)
+            {
+                node.AddValue("targetBody", targetBody.name);
+            }
+            node.AddValue("activeVessel", activeVessel);
+            node.AddValue("minCount", minCount);
+            node.AddValue("maxCount", maxCount);
+            if (antennaType != null)
+            {
+                node.AddValue("antennaType", antennaType);
+            }
+        }
+
+        protected override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            title = ConfigNodeUtil.ParseValue<string>(node, "title", (string)null);
+            minRange = ConfigNodeUtil.ParseValue<double>(node, "minRange");
+            maxRange = ConfigNodeUtil.ParseValue<double>(node, "maxRange", double.MaxValue);
+            targetBody = ConfigNodeUtil.ParseValue<CelestialBody>(node, "targetBody", (CelestialBody)null);
+            activeVessel = ConfigNodeUtil.ParseValue<bool>(node, "activeVessel");
+            minCount = ConfigNodeUtil.ParseValue<int>(node, "minCount");
+            maxCount = ConfigNodeUtil.ParseValue<int>(node, "maxCount");
+            antennaType = ConfigNodeUtil.ParseValue<AntennaType?>(node, "antennaType", (AntennaType?)null);
+        }
+
+        /// <summary>
+        /// Whether this vessel meets the parameter condition.
+        /// </summary>
+        /// <param name="vessel">Vessel to check</param>
+        /// <returns>Whether the vessel meets the parameter condition(s).</returns>
+        protected override bool VesselMeetsCondition(Vessel vessel)
+        {
+            LoggingUtil.LogVerbose(this, "Checking VesselMeetsCondition: " + vessel.id);
+
+            // Get all the antennae
+            VesselSatellite sat = RTCore.Instance.Satellites[vessel.id];
+            IEnumerable<IAntenna> antennae = sat.Antennas.Where(a => a.Activated && a.Powered);
+
+            // Filter by type
+            if (antennaType == AntennaType.Dish)
+            {
+                antennae = antennae.Where(a => a.Dish > 0.0);
+            }
+            else if (antennaType == AntennaType.Omni)
+            {
+                antennae = antennae.Where(a => a.Omni > 0.0);
+            }
+
+            // Filter for active vessel
+            if (activeVessel)
+            {
+                antennae = antennae.Where(a => a.Target == NetworkManager.ActiveVesselGuid || a.Omni > 0.0);
+            }
+
+            double minRange = this.minRange;
+
+            // Filter for celestial bodies
+            if (targetBody != null)
+            {
+                double distance = (Planetarium.fetch.Home.position - targetBody.position).magnitude;
+                antennae = antennae.Where(a => a.Target == targetBody.Guid() || a.Omni > 0.0);
+                minRange = Math.Max(minRange, distance);
+            }
+
+            // Filter for range
+            antennae = antennae.Where(a => Math.Max(a.Omni, a.Dish) >= minRange && Math.Max(a.Omni, a.Dish) <= maxRange);
+
+            // Validate count
+            int count = antennae.Count();
+            return count >= minCount && count <= maxCount;
+        }
+    }
+}
