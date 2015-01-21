36419316267789b86599c8d2ecf14558dfeb9ebf
Jeremie Rossignol <jrossignol@gmail.com>
2015-01-21 09:34:12 -0700

    Fixes to assembly versions.

--- source/CC_SCANsat/Properties/AssemblyInfo.cs
+++ source/CC_SCANsat/Properties/AssemblyInfo.cs
@@ -33,8 +33,9 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.6.0")]
+[assembly: AssemblyVersion("0.6")]
 [assembly: AssemblyFileVersion("0.6.0")]
+[assembly: AssemblyInformationalVersion("0.6.0")]
 [assembly: KSPAssembly("CC_SCANsat", 0, 6)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 0, 6)]
 [assembly: KSPAssemblyDependency("SCANsat", 0, 9)]
