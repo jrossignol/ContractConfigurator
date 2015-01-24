6969a43686025b77a2e4dc8bf6d9b00ee70ef06d
Jeremie Rossignol <jrossignol@gmail.com>
2015-01-23 21:54:41 -0700

    [#112] Added ExperimentalPart behaviour.

---
+++ source/ContractConfigurator/BehaviourFactory/ExperimentalPartFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using ContractConfigurator;
+
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// BehaviourFactory wrapper for ExperimentalPart ContractBehaviour.
+    /// </summary>
+    public class ExperimentalPartFactory : BehaviourFactory
+    {
+        protected AvailablePart part;
+        protected bool add;
+        protected bool remove;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<AvailablePart>(configNode, "part", ref part, this);
+            valid &= ConfigNodeUtil.ParseValue<bool>(configNode, "add", ref add, this, true);
+            valid &= ConfigNodeUtil.ParseValue<bool>(configNode, "remove", ref remove, this, true);
+
+            return valid;
+        }
+
+        public override ContractBehaviour Generate(ConfiguredContract contract)
+        {
+            return new ExperimentalPart(part, add, remove);
+        }
+    }
+}
---
+++ source/ContractConfigurator/ContractBehaviour/ExperimentalPart.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using ContractConfigurator;
+
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// Behaviour for adding/removing an experimental part.
+    /// </summary>
+    public class ExperimentalPart : ContractBehaviour
+    {
+        protected AvailablePart part;
+        protected bool add;
+        protected bool remove;
+
+        public ExperimentalPart()
+            : base()
+        {
+        }
+
+        public ExperimentalPart(AvailablePart part, bool add, bool remove)
+        {
+            this.part = part;
+            this.add = add;
+            this.remove = remove;
+        }
+
+        protected override void OnAccepted()
+        {
+            if (add)
+            {
+                ResearchAndDevelopment.AddExperimentalPart(part);
+            }
+        }
+
+        protected override void OnCancelled()
+        {
+            if (add)
+            {
+                ResearchAndDevelopment.RemoveExperimentalPart(part);
+            }
+        }
+
+        protected override void OnDeadlineExpired()
+        {
+            if (add)
+            {
+                ResearchAndDevelopment.RemoveExperimentalPart(part);
+            }
+        }
+
+        protected override void OnFailed()
+        {
+            if (add)
+            {
+                ResearchAndDevelopment.RemoveExperimentalPart(part);
+            }
+        }
+
+        protected override void OnCompleted()
+        {
+            if (remove)
+            {
+                ResearchAndDevelopment.RemoveExperimentalPart(part);
+            }
+        }
+
+        protected override void OnSave(ConfigNode configNode)
+        {
+            configNode.AddValue("part", part.name);
+            configNode.AddValue("add", add);
+            configNode.AddValue("remove", remove);
+        }
+
+        protected override void OnLoad(ConfigNode configNode)
+        {
+            part = ConfigNodeUtil.ParseValue<AvailablePart>(configNode, "part");
+            add = ConfigNodeUtil.ParseValue<bool>(configNode, "add");
+            remove = ConfigNodeUtil.ParseValue<bool>(configNode, "remove");
+        }
+    }
+}
