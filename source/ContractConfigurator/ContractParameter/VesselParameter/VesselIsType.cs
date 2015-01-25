da7fcbd365111d4aa853722b8e886a60641f125f
Jeremie Rossignol <jrossignol@gmail.com>
2015-01-24 23:01:58 -0700

    VesselIsType

---
+++ source/ContractConfigurator/ContractParameter/VesselParameter/VesselIsType.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+
+namespace ContractConfigurator.Parameters
+{
+    /// <summary>
+    /// Parameter for checking the VesselType of vessel.
+    /// </summary>
+    public class VesselIsType : VesselParameter
+    {
+        protected string title { get; set; }
+        protected VesselType vesselType { get; set; }
+
+        public VesselIsType()
+            : base()
+        {
+        }
+
+        public VesselIsType(VesselType vesselType, string title = null)
+            : base()
+        {
+            this.vesselType = vesselType;
+            this.title = title;
+        }
+
+        protected override string GetTitle()
+        {
+            string output = "";
+            if (string.IsNullOrEmpty(title))
+            {
+                output = "Vessel type: " + vesselType;
+            }
+            else
+            {
+                output = title;
+            }
+            return output;
+        }
+
+        protected override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+            node.AddValue("title", title);
+            node.AddValue("vesselType", vesselType);
+        }
+
+        protected override void OnLoad(ConfigNode node)
+        {
+            base.OnLoad(node);
+            title = node.GetValue("title");
+            vesselType = ConfigNodeUtil.ParseValue<VesselType>(node, "vesselType");
+        }
+
+        protected override void OnRegister()
+        {
+            base.OnRegister();
+            GameEvents.onVesselWasModified.Add(new EventData<Vessel>.OnEvent(OnVesselWasModified));
+        }
+
+        protected override void OnUnregister()
+        {
+            base.OnUnregister();
+            GameEvents.onVesselWasModified.Remove(new EventData<Vessel>.OnEvent(OnVesselWasModified));
+        }
+
+        protected override void OnFlightReady()
+        {
+ 	        base.OnFlightReady();
+            CheckVessel(FlightGlobals.ActiveVessel);
+        }
+
+        protected override void OnVesselCreate(Vessel vessel)
+        {
+            base.OnVesselCreate(vessel);
+            CheckVessel(vessel);
+        }
+
+        protected override void OnVesselChange(Vessel vessel)
+        {
+            base.OnVesselChange(vessel);
+            CheckVessel(vessel);
+        }
+
+        protected void OnVesselWasModified(Vessel vessel)
+        {
+            CheckVessel(vessel);
+        }
+
+        /// <summary>
+        /// Whether this vessel meets the parameter condition.
+        /// </summary>
+        /// <param name="vessel">The vessel to check</param>
+        /// <returns>Whether the vessel meets the condition</returns>
+        protected override bool VesselMeetsCondition(Vessel vessel)
+        {
+            LoggingUtil.LogVerbose(this, "Checking VesselMeetsCondition: " + vessel.id);
+            return vessel.vesselType == vesselType;
+        }
+    }
+}
---
+++ source/ContractConfigurator/ParameterFactory/VesselIsTypeFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using ContractConfigurator.Parameters;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ParameterFactory wrapper for VesselIsType ContractParameter.
+    /// </summary>
+    public class VesselIsTypeFactory : ParameterFactory
+    {
+        protected VesselType vesselType;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<VesselType>(configNode, "vesselType", ref vesselType, this);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new VesselIsType(vesselType, title);
+        }
+    }
+}
