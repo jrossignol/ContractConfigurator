f6410e178b48583a6bd39f3c468b67bcf844563a
Jeremie Rossignol <jrossignol@gmail.com>
2015-01-28 21:44:38 -0700

    Minor changes for debug window.

--- source/ContractConfigurator/ContractConfigurator.cs
+++ source/ContractConfigurator/ContractConfigurator.cs
@@ -130,25 +130,6 @@ namespace ContractConfigurator
                     WindowGUI,
                     "Contract Configurator " + ainfoV.InformationalVersion);
             }
-
-            // TODO - remove
-            if (FlightGlobals.ActiveVessel != null && FlightGlobals.ActiveVessel.mainBody.pqsController != null)
-            {
-                // Calculate the distance
-                if (Time.frameCount % 500 == 0)
-                {
-                    Vessel vessel = FlightGlobals.ActiveVessel;
-                    // Figure out the terrain height
-                    double latRads = Math.PI / 180.0 * vessel.latitude;
-                    double lonRads = Math.PI / 180.0 * vessel.longitude;
-                    Vector3d radialVector = new Vector3d(Math.Cos(latRads) * Math.Cos(lonRads), Math.Sin(latRads), Math.Cos(latRads) * Math.Sin(lonRads));
-                    double height = Math.Max(vessel.mainBody.pqsController.GetSurfaceHeight(radialVector) - vessel.mainBody.pqsController.radius, 0.0);
-
-                    LoggingUtil.LogVerbose(this, "latitude = " + vessel.latitude);
-                    LoggingUtil.LogVerbose(this, "longitude = " + vessel.longitude);
-                    LoggingUtil.LogVerbose(this, "altitude = " + (vessel.altitude - height));
-                }
-            }
         }
 
         protected void LoadTextures()
@@ -179,6 +160,8 @@ namespace ContractConfigurator
             GUILayout.BeginHorizontal();
 
             GUILayout.BeginVertical(GUILayout.Width(500));
+
+            GUILayout.Label("Sucessfully loaded " + successContracts + " out of " + totalContracts + " contracts.", yellowLabel);
 
             GUILayout.BeginHorizontal();
             if (GUILayout.Button("Reload Contracts"))
