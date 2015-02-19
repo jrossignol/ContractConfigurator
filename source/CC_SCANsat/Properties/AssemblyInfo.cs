45f0881942a0af16821035d87377c94d84ac8127
Jeremie Rossignol <jrossignol@gmail.com>
2015-02-18 22:53:22 -0700

    0.6.6

--- source/CC_SCANsat/Properties/AssemblyInfo.cs
+++ source/CC_SCANsat/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("0.6.5")]
-[assembly: AssemblyInformationalVersion("0.6.5")]
+[assembly: AssemblyFileVersion("0.6.6")]
+[assembly: AssemblyInformationalVersion("0.6.6")]
 [assembly: KSPAssembly("CC_SCANsat", 1, 0)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
 [assembly: KSPAssemblyDependency("SCANsat", 1, 0)]
