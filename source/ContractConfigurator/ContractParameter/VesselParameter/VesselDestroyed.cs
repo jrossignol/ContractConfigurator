ab6d45ec873d33029228b29d208d2540610f1032
Jeremie Rossignol <jrossignol@gmail.com>
2015-03-14 21:39:55 -0600

    Added VesselDestroyed

---
+++ source/ContractConfigurator/ContractParameter/VesselParameter/VesselDestroyed.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+
+namespace ContractConfigurator.Parameters
+{
+    /// <summary>
+    /// Parameter for checking that a vessel gets destroyed.
+    /// </summary>
+    public class VesselDestroyed : VesselParameter
+    {
+        public VesselDestroyed()
+            : base(null)
+        {
+        }
+
+        public VesselDestroyed(string title)
+            : base(title)
+        {
+        }
+
+        protected override string GetTitle()
+        {
+            string output = "";
+            if (string.IsNullOrEmpty(title))
+            {
+                output = "Vessel Destroyed";
+            }
+            else
+            {
+                output = title;
+            }
+            return output;
+        }
+
+        protected override void OnParameterSave(ConfigNode node)
+        {
+            base.OnParameterSave(node);
+        }
+
+        protected override void OnParameterLoad(ConfigNode node)
+        {
+            base.OnParameterLoad(node);
+        }
+
+        protected override void OnRegister()
+        {
+            base.OnRegister();
+            GameEvents.onVesselWillDestroy.Add(new EventData<Vessel>.OnEvent(OnVesselWillDestroy));
+        }
+
+        protected override void OnUnregister()
+        {
+            base.OnUnregister();
+            GameEvents.onVesselWillDestroy.Remove(new EventData<Vessel>.OnEvent(OnVesselWillDestroy));
+        }
+
+        protected virtual void OnVesselWillDestroy(Vessel v)
+        {
+            LoggingUtil.LogVerbose(this, "OnVesselWillDestroy: " + v.id);
+            SetState(v, ParameterState.Complete);
+            CheckVessel(v);
+        }
+
+        /// <summary>
+        /// Whether this vessel meets the parameter condition.
+        /// </summary>
+        /// <param name="vessel">The vessel to check</param>
+        /// <returns>Always true</returns>
+        protected override bool VesselMeetsCondition(Vessel vessel)
+        {
+            return GetStateForVessel(vessel) == ParameterState.Complete;
+        }
+    }
+}
---
+++ source/ContractConfigurator/ParameterFactory/VesselDestroyed.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using ContractConfigurator.Parameters;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ParameterFactory wrapper for VesselDestroyed ContractParameter. 
+    /// </summary>
+    public class VesselDestroyedFactory : ParameterFactory
+    {
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new VesselDestroyed(title);
+        }
+    }
+}
