2d010e58644606b656874268148bbc2ff6b7b0b6
Jeremie Rossignol <jrossignol@gmail.com>
2015-04-01 21:58:20 -0600

    Fix referenced version.

--- source/ContractConfigurator/ParameterFactory/IsNotVesselFactory.cs
+++ source/ContractConfigurator/ParameterFactory/IsNotVesselFactory.cs
@@ -24,7 +24,7 @@ namespace ContractConfigurator
 
             if (configNode.HasValue("vesselKey"))
             {
-                LoggingUtil.LogWarning(this, "The 'vesselKey' attribute is obsolete as of Contract Configurator 0.7.4.  It will be removed in 1.0.0 in favour of the vessel attribute.");
+                LoggingUtil.LogWarning(this, "The 'vesselKey' attribute is obsolete as of Contract Configurator 0.7.5.  It will be removed in 1.0.0 in favour of the vessel attribute.");
                 valid &= ConfigNodeUtil.ParseValue<string>(configNode, "vesselKey", x => vesselKey = x, this);
             }
             else
