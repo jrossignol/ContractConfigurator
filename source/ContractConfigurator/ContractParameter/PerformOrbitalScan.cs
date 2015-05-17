2ad87e1b11f1fedda11ba18196c95ced827e7e44
Jeremie Rossignol <jrossignol@gmail.com>
2015-05-17 13:18:59 -0600

    Added PerformOrbitalScan parameter.

---
+++ source/ContractConfigurator/ContractParameter/PerformOrbitalScan.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+
+namespace ContractConfigurator.Parameters
+{
+    /// <summary>
+    /// Parameter requiring that an orbital resource scan is done
+    /// </summary>
+    public class PerformOrbitalScan : ContractConfiguratorParameter
+    {
+        protected CelestialBody targetBody;
+
+        private double lastUpdate = 0.0;
+
+        public PerformOrbitalScan()
+            : base(null)
+        {
+        }
+
+        public PerformOrbitalScan(string title,  CelestialBody targetBody)
+            : base(title)
+        {
+            disableOnStateChange = true;
+
+            this.targetBody = targetBody;
+        }
+
+        protected override string GetTitle()
+        {
+            string output;
+            if (string.IsNullOrEmpty(title))
+            {
+                output = "Perform an orbital resource scan of " + targetBody.theName;
+            }
+            else 
+            {
+                output = title;
+            }
+            return output;
+        }
+
+        protected override void OnParameterSave(ConfigNode node)
+        {
+            node.AddValue("targetBody", targetBody.name);
+        }
+
+        protected override void OnParameterLoad(ConfigNode node)
+        {
+            targetBody = ConfigNodeUtil.ParseValue<CelestialBody>(node, "targetBody");
+        }
+
+        protected override void OnUpdate()
+        {
+            base.OnUpdate();
+
+            // Do a check every second or so
+            if (Planetarium.GetUniversalTime() - lastUpdate > 1.0f)
+            {
+                lastUpdate = Planetarium.GetUniversalTime();
+
+                if (ResourceScenario.Instance != null &&
+                    ResourceScenario.Instance.gameSettings.GetPlanetScanInfo().Where(psd => psd.PlanetId == targetBody.flightGlobalsIndex).Any())
+                {
+                    SetState(ParameterState.Complete);
+                }
+            }
+        }
+    }
+}
---
+++ source/ContractConfigurator/ParameterFactory/PerformOrbitalScanFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using ContractConfigurator.Parameters;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ParameterFactory wrapper for PerformOrbitalScan ContractParameter.
+    /// </summary>
+    public class PerformOrbitalScanFactory : ParameterFactory
+    {
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            // Validate target body
+            valid &= ValidateTargetBody(configNode);
+
+            return valid;
+        }
+
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new PerformOrbitalScan(title, targetBody);
+        }
+    }
+}
