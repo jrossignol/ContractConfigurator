7ccda8f3fe446afd3f17b944181f5b0ef7a120cc
Jeremie Rossignol <jrossignol@gmail.com>
2015-05-17 13:22:24 -0600

    PerformOrbitalScan requirement.

---
+++ source/ContractConfigurator/ContractRequirement/PerformOrbitalScanRequirement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using KSPAchievements;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ContractRequirement for performing an orbital scan of a celestial body.
+    /// </summary>
+    public class PerformOrbitalScanRequirement : ContractRequirement
+    {
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ValidateTargetBody(configNode);
+
+            return valid;
+        }
+
+        public override bool RequirementMet(ConfiguredContract contract)
+        {
+            if (ResourceScenario.Instance == null)
+            {
+                return false;
+            }
+
+            return ResourceScenario.Instance.gameSettings.GetPlanetScanInfo().Where(psd => psd.PlanetId == targetBody.flightGlobalsIndex).Any();
+        }
+    }
+}
