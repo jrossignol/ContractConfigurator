4e9073aa701300d059b15c16377c1f3ac7fbf65b
Jeremie Rossignol <jrossignol@gmail.com>
2015-05-17 23:22:25 -0600

    No random contracts!

---
+++ source/ContractConfigurator/Other/NoRandomContractMentality.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Contracts.Agents;
+
+namespace ContractConfigurator.Other
+{
+    /// <summary>
+    /// Mentality that can be used to prevent random contracts from being assigned to the agent.
+    /// </summary>
+    public class NoRandomContractMentality : AgentMentality
+    {
+        public override bool CanProcessContract(Contracts.Contract contract)
+        {
+            // Clean house
+            foreach (Agent agent in AgentList.Instance.Agencies)
+            {
+                if (agent.Mentality.Contains(this) && agent.Mentality.Count > 1)
+                {
+                    agent.Mentality.RemoveAll(am => am != this);
+                }
+            }
+            return false;
+        }
+
+        public override KeywordScore ScoreKeyword(string keyword)
+        {
+            return KeywordScore.NegativeHigh;
+        }
+    }
+}
