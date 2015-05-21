4e91320cf038d7aaa4028c30bd7d710829e5f04c
Jeremie Rossignol <jrossignol@gmail.com>
2015-05-21 11:49:42 -0600

    ExperimentSituationParser.

---
+++ source/ContractConfigurator/ExpressionParser/Parsers/Classes/Science/ExperimentSituationParser.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using ContractConfigurator.Util;
+
+namespace ContractConfigurator.ExpressionParser
+{
+    /// <summary>
+    /// Expression parser subclass for Experiment.
+    /// </summary>
+    public class ExperimentSituationParser : EnumExpressionParser<ExperimentSituations>, IExpressionParserRegistrer
+    {
+        public void RegisterExpressionParsers()
+        {
+            RegisterParserType(typeof(ExperimentSituations), typeof(ExperimentSituationParser));
+        }
+
+        public ExperimentSituationParser()
+        {
+        }
+
+        internal override U ConvertType<U>(ExperimentSituations value)
+        {
+            if (typeof(U) == typeof(string))
+            {
+                return (U)(object)value.Print();
+            }
+            return base.ConvertType<U>(value);
+        }
+    }
+}
