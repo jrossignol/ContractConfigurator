483d81bcf8757bca27ea8014c7c918125b91a398
Jeremie Rossignol <jrossignol@gmail.com>
2015-05-28 08:59:25 -0600

    Added UnlockTech.

---
+++ source/ContractConfigurator/BehaviourFactory/UnlockTechFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using ContractConfigurator;
+using ContractConfigurator.ExpressionParser;
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// BehaviourFactory wrapper for UnlockTech ContractBehaviour.
+    /// Author: Klefenz
+    /// </summary>
+    public class UnlockTechFactory : BehaviourFactory
+    {
+        protected List<string> techID;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "techID", x => techID = x, this);
+
+            return valid;
+        }
+
+        public override ContractBehaviour Generate(ConfiguredContract contract)
+        {
+            return new UnlockTech(techID);
+        }
+    }
+}
---
+++ source/ContractConfigurator/ContractBehaviour/UnlockTech.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using ContractConfigurator;
+
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// Behaviour for ulocking a tech node on contract/parameter completion/failure.
+    /// Author: Klefenz
+    /// </summary>
+    public class UnlockTech : ContractBehaviour
+    {
+        protected List<string> techID;    //the tech to be unlocked
+
+        public UnlockTech()
+            : base()
+        {
+        }
+
+        public UnlockTech(IEnumerable<string> techID)
+        {
+            this.techID = techID.ToList();
+        }
+
+        protected override void OnCompleted()
+        {
+            foreach (string tech in techID)
+            {
+                UnlockTechnology(tech);
+            }
+        }
+
+        protected void UnlockTechnology(string techID)
+        {
+            ProtoTechNode ptd = new ProtoTechNode();
+            ptd.state = RDTech.State.Available;
+            ptd.techID = techID;
+            ptd.scienceCost = 9999;
+
+            if (HighLogic.CurrentGame.Parameters.Difficulty.BypassEntryPurchaseAfterResearch)
+            {
+                ptd.partsPurchased = PartLoader.Instance.parts.Where(p => p.TechRequired == techID).ToList();
+            }
+            else
+            {
+                ptd.partsPurchased = new List<AvailablePart>();
+            }
+                
+            ResearchAndDevelopment.Instance.SetTechState(techID, ptd);
+        }
+
+        protected override void OnSave(ConfigNode configNode)
+        {
+            foreach (string tech in techID)
+            {
+                configNode.AddValue("techID", techID);
+            }
+        }
+
+        protected override void OnLoad(ConfigNode configNode)
+        {
+            techID = ConfigNodeUtil.ParseValue<List<string>>(configNode, "techID");
+        }
+    }
+}
