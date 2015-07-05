7c075a992c0e1d30b8fd9c9cd8c4d29bb31dd8a6
Jeremie Rossignol <jrossignol@gmail.com>
2015-07-05 13:14:40 -0600

    [#44] Added ChangeVesselOwnership contract behaviour.

---
+++ source/ContractConfigurator/BehaviourFactory/ChangeVesselOwnershipFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// BehaviourFactory wrapper for ChangeVesselOwnership ContractBehaviour.
+    /// </summary>
+    public class ChangeVesselOwnershipFactory : BehaviourFactory
+    {
+        private ChangeVesselOwnership.State onState;
+        private List<string> vessels;
+        private bool owned;
+        private List<string> parameter;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            valid &= ConfigNodeUtil.ParseValue<ChangeVesselOwnership.State>(configNode, "onState", x => onState = x, this, ChangeVesselOwnership.State.ParameterCompleted);
+            valid &= ConfigNodeUtil.ParseValue<bool?>(configNode, "owned", x => owned = x.Value, this, (bool?)true);
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "vessel", x => vessels = x, this);
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "parameter", x => parameter = x, this, new List<string>());
+
+            return valid;
+        }
+
+        public override ContractBehaviour Generate(ConfiguredContract contract)
+        {
+            return new ChangeVesselOwnership(onState, vessels, owned, parameter);
+        }
+    }
+}
---
+++ source/ContractConfigurator/ContractBehaviour/ChangeVesselOwnership.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using ContractConfigurator;
+using ContractConfigurator.Parameters;
+using ContractConfigurator.ExpressionParser;
+
+namespace ContractConfigurator.Behaviour
+{
+    /// <summary>
+    /// Behaviour for changing vessel ownership.
+    /// </summary>
+    public class ChangeVesselOwnership : ContractBehaviour
+    {
+        public enum State
+        {
+            ContractAccepted,
+            ContractCompletedFailure,
+            ContractCompletedSuccess,
+            ParameterCompleted,
+        }
+
+        private State onState;
+        private List<string> vessels;
+        private bool owned;
+        private List<string> parameter;
+
+        public ChangeVesselOwnership()
+        {
+        }
+
+        public ChangeVesselOwnership(State onState, List<string> vessels, bool owned, List<string> parameter)
+        {
+            this.onState = onState;
+            this.vessels = vessels;
+            this.owned = owned;
+            this.parameter = parameter;
+        }
+
+        private void SetVessels()
+        {
+            foreach (Vessel vessel in vessels.Select(v => ContractVesselTracker.Instance.GetAssociatedVessel(v)))
+            {
+                if (vessel != null)
+                {
+                    vessel.DiscoveryInfo.SetLevel(owned ? DiscoveryLevels.Owned : DiscoveryLevels.Unowned);
+                }
+            }
+        }
+
+        protected override void OnAccepted()
+        {
+            if (onState == State.ContractAccepted)
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnCancelled()
+        {
+            if (onState == State.ContractCompletedFailure)
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnDeadlineExpired()
+        {
+            if (onState == State.ContractCompletedFailure)
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnFailed()
+        {
+            if (onState == State.ContractCompletedFailure)
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnCompleted()
+        {
+            if (onState == State.ContractCompletedSuccess)
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnParameterStateChange(ContractParameter param)
+        {
+            if (onState == State.ParameterCompleted && param.State == ParameterState.Complete && parameter.Contains(param.ID))
+            {
+                SetVessels();
+            }
+        }
+
+        protected override void OnLoad(ConfigNode configNode)
+        {
+            onState = ConfigNodeUtil.ParseValue<State>(configNode, "onState");
+            owned = ConfigNodeUtil.ParseValue<bool>(configNode, "owned");
+            vessels = ConfigNodeUtil.ParseValue<List<string>>(configNode, "vessel", new List<string>());
+            parameter = ConfigNodeUtil.ParseValue<List<string>>(configNode, "parameter", new List<string>());
+        }
+
+        protected override void OnSave(ConfigNode configNode)
+        {
+            configNode.AddValue("onState", onState);
+            configNode.AddValue("owned", owned);
+            foreach (string v in vessels)
+            {
+                configNode.AddValue("vessel", v);
+            }
+            foreach (string p in parameter)
+            {
+                configNode.AddValue("parameter", p);
+            }
+        }
+    }
+}
