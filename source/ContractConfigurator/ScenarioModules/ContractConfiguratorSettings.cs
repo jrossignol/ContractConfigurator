d92e12ce9de73a63764e8baf411c886250dc064f
Jeremie Rossignol <jrossignol@gmail.com>
2015-07-21 21:25:55 -0600

    WIP settings view. :xa

--- source/ContractConfigurator/ScenarioModules/ContractConfiguratorSettings.cs
+++ source/ContractConfigurator/ScenarioModules/ContractConfiguratorSettings.cs
@@ -9,23 +9,35 @@ using KSP;
 namespace ContractConfigurator
 {
     [KSPScenario(ScenarioCreationOptions.AddToExistingCareerGames | ScenarioCreationOptions.AddToNewCareerGames,
-        GameScenes.SPACECENTER)]
+        GameScenes.SPACECENTER, GameScenes.FLIGHT, GameScenes.TRACKSTATION)]
     public class ContractConfiguratorSettings : ScenarioModule
     {
+        #region ContractGroupDetails
+        private enum ContractGroupState
+        {
+            DEFAULT,
+            ENABLED,
+            DISABLED
+        }
+
+        private class ContractGroupDetails
+        {
+            public ContractGroup group;
+            public bool enabled = true;
+            public bool expanded = false;
+
+            public ContractGroupDetails(ContractGroup group)
+            {
+                this.group = group;
+            }
+        }
+        Dictionary<ContractGroup, ContractGroupDetails> contractGroupDetails = new Dictionary<ContractGroup, ContractGroupDetails>();
+        #endregion
+
         public static ContractConfiguratorSettings Instance { get; private set; }
 
         private ApplicationLauncherButton launcherButton = null;
         
-        private Rect windowPos = new Rect(580f, 40f, 1f, 1f);
-        private bool showGUI = false;
-        private static IEnumerable<ContractType> guiContracts;
-
-        private static Texture2D closeIcon;
-        private static Texture2D toolbarIcon;
-
-        private bool stylesSetup = false;
-        private GUIStyle windowStyle;
-
         public ContractConfiguratorSettings()
         {
             Debug.Log("ContractConfiguratorSettings()");
@@ -52,6 +64,47 @@ namespace ContractConfigurator
             GameEvents.onGUIApplicationLauncherReady.Remove(new EventVoid.OnEvent(SetupToolbar));
             GameEvents.onGUIApplicationLauncherUnreadifying.Remove(new EventData<GameScenes>.OnEvent(TeardownToolbar));
         }
+
+        #region Styles
+        private bool stylesSetup = false;
+
+        private static GUIStyle regularText;
+        private static GUIStyle disabledText;
+        private static GUIStyle toggleStyle;
+        private GUIStyle tipStyle;
+
+        private void SetupStyles()
+        {
+            stylesSetup = true;
+
+            regularText = new GUIStyle(GUI.skin.label);
+            regularText.padding = new RectOffset(0, 0, 2, 2);
+            regularText.normal.textColor = Color.white;
+
+            disabledText = new GUIStyle(regularText);
+            disabledText.normal.textColor = Color.grey;
+
+            toggleStyle = new GUIStyle(GUI.skin.toggle);
+
+            // Tooltips
+            tipStyle = new GUIStyle(GUI.skin.box);
+            tipStyle.wordWrap = true;
+            tipStyle.stretchHeight = true;
+            tipStyle.normal.textColor = Color.white;
+        }
+        #endregion
+
+        #region GUI
+        private Rect windowPos = new Rect(580f, 40f, 1f, 1f);
+        private bool showGUI = false;
+        private static IEnumerable<ContractType> guiContracts;
+
+        private Rect tooltipPosition;
+        private string toolTip;
+        private double toolTipTime;
+
+        private static Texture2D closeIcon;
+        private static Texture2D toolbarIcon;
 
         private void ToggleWindow()
         {
@@ -83,25 +136,31 @@ namespace ContractConfigurator
         {
             if (showGUI)
             {
-                GUI.skin = HighLogic.Skin;
-
                 if (!stylesSetup)
                 {
-                    stylesSetup = true;
-                    windowStyle = new GUIStyle(GUI.skin.window);
-                    windowStyle.normal.textColor = Color.white;
+                    SetupStyles();
                 }
 
-                GUI.skin.window = windowStyle;
+                GUI.skin = HighLogic.Skin;
 
-                   var ainfoV = Attribute.GetCustomAttribute(typeof(ContractConfigurator).Assembly, typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute;
+                var ainfoV = Attribute.GetCustomAttribute(typeof(ContractConfigurator).Assembly, typeof(AssemblyInformationalVersionAttribute)) as AssemblyInformationalVersionAttribute;
 
-                windowPos.xMin = Screen.width - 300 - 12;
+                windowPos.xMin = Screen.width - 300 - 14;
                 windowPos = GUILayout.Window(
                     typeof(ContractConfiguratorSettings).FullName.GetHashCode(),
                     windowPos,
                     WindowGUI,
                     "Contract Configurator " + ainfoV.InformationalVersion + " Settings");
+
+                if (Event.current.type == EventType.Repaint && GUI.tooltip != toolTip)
+                {
+                    toolTipTime = Time.fixedTime;
+                    toolTip = GUI.tooltip;
+                    Debug.Log("set tool tip: " + toolTip);
+                }
+
+                GUI.depth = 0;
+                DrawToolTip();
             }
         }
 
@@ -114,15 +173,90 @@ namespace ContractConfigurator
                 guiContracts = ContractType.AllContractTypes;
             }
             
-            foreach (ContractGroup contractGroup in ContractGroup.AllGroups.Where(g => g == null || g.parent == null))
+            foreach (ContractGroup contractGroup in ContractGroup.AllGroups.Where(g => g == null || g.parent == null).OrderBy(g => g == null ? "ZZZ" : g.name))
             {
                 if (guiContracts.Any(ct => contractGroup == null ? ct.group == null : contractGroup.BelongsToGroup(ct)))
                 {
-                    GUILayout.Label(contractGroup == null ? "No Group" : contractGroup.name);
+                    ContractGroupLine(contractGroup);
                 }
             }
 
             GUILayout.EndVertical();
         }
+
+        private void ContractGroupLine(ContractGroup contractGroup)
+        {
+            if (!contractGroupDetails.ContainsKey(contractGroup))
+            {
+                contractGroupDetails[contractGroup] = new ContractGroupDetails(contractGroup);
+            }
+            ContractGroupDetails details = contractGroupDetails[contractGroup];
+
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button(details.expanded ? "-" : "+", GUILayout.Width(20), GUILayout.Height(20)))
+            {
+                details.expanded = !details.expanded;
+            }
+            GUILayout.Label(contractGroup == null ? "No Group" : contractGroup.name, details.enabled ? regularText : disabledText, GUILayout.ExpandWidth(true));
+            details.enabled = GUILayout.Toggle(details.enabled, "", toggleStyle, GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+        }
+
+        /// <summary>
+        /// Draw tool tips.
+        /// </summary>
+        private void DrawToolTip()
+        {
+            if (!string.IsNullOrEmpty(toolTip))
+            {
+                if (Time.fixedTime > toolTipTime + 0.5)
+                {
+                    GUIContent tip = new GUIContent(toolTip);
+
+                    Vector2 textDimensions = GUI.skin.box.CalcSize(tip);
+                    if (textDimensions.x > 240)
+                    {
+                        textDimensions.x = 240;
+                        textDimensions.y = tipStyle.CalcHeight(tip, 240);
+                    }
+                    tooltipPosition.width = textDimensions.x;
+                    tooltipPosition.height = textDimensions.y;
+                    tooltipPosition.x = Event.current.mousePosition.x + tooltipPosition.width > Screen.width ?
+                        Screen.width - tooltipPosition.width : Event.current.mousePosition.x;
+                    tooltipPosition.y = Event.current.mousePosition.y + 20;
+
+                    GUI.Label(tooltipPosition, tip, tipStyle);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Settings
+
+        public override void OnSave(ConfigNode node)
+        {
+            foreach (ContractGroupDetails details in contractGroupDetails.Values)
+            {
+                ConfigNode groupNode = new ConfigNode("CONTRACT_GROUP");
+                node.AddNode(groupNode);
+
+                groupNode.AddValue("group", details.group.name);
+                groupNode.AddValue("enabled", details.enabled);
+            }
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            foreach (ConfigNode groupNode in node.GetNodes("CONTRACT_GROUP"))
+            {
+                ContractGroup group = ConfigNodeUtil.ParseValue<ContractGroup>(groupNode, "group");
+
+                ContractGroupDetails details = new ContractGroupDetails(group);
+                details.enabled = ConfigNodeUtil.ParseValue<bool>(groupNode, "enabled");
+            }
+        }
+
+        #endregion
     }
 }
