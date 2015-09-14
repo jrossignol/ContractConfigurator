d030812f2f32ae22b2b2e657c964df0236fe5b1e
Jeremie Rossignol <jrossignol@gmail.com>
2015-09-14 10:57:34 -0600

    [#316] FacilityRequirement now works outside the space center.

--- source/ContractConfigurator/ContractRequirement/FacilityRequirement.cs
+++ source/ContractConfigurator/ContractRequirement/FacilityRequirement.cs
@@ -14,7 +14,7 @@ namespace ContractConfigurator
     /// </summary>
     public class FacilityRequirement : ContractRequirement
     {
-        protected string facility;
+        protected SpaceCenterFacility facility;
         protected int minLevel;
         protected int maxLevel;
 
@@ -26,7 +26,7 @@ namespace ContractConfigurator
             // Check on active contracts too
             checkOnActiveContract = configNode.HasValue("checkOnActiveContract") ? checkOnActiveContract : true;
 
-            valid &= ConfigNodeUtil.ParseValue<string>(configNode, "facility", x => facility = x, this);
+            valid &= ConfigNodeUtil.ParseValue<SpaceCenterFacility>(configNode, "facility", x => facility = x, this);
             valid &= ConfigNodeUtil.ParseValue<int>(configNode, "minLevel", x => minLevel = x, this, 1, x => Validation.Between(x, 0, 3));
             valid &= ConfigNodeUtil.ParseValue<int>(configNode, "maxLevel", x => maxLevel = x, this, 3, x => Validation.Between(x, 0, 3));
             valid &= ConfigNodeUtil.AtLeastOne(configNode, new string[] { "minLevel", "maxLevel" }, this);
@@ -36,22 +36,8 @@ namespace ContractConfigurator
 
         public override bool RequirementMet(ConfiguredContract contract)
         {
-            IEnumerable<UpgradeableFacility> facilities = UnityEngine.Object.FindObjectsOfType<UpgradeableFacility>().
-                Where<UpgradeableFacility>(f => f.name == facility);
-
-            if (facilities.Count() > 0)
-            {
-                UpgradeableFacility upgradeableFacility = facilities.First<UpgradeableFacility>();
-
-                int level = upgradeableFacility.FacilityLevel;
-                return level >= minLevel && level <= maxLevel;
-            }
-            else
-            {
-                // Should only get here if the facility name entered was bad
-                LoggingUtil.LogError(this, "Coudn't find facility with name '" + facility + "'!");
-                return false;
-            }
+            int level = ScenarioUpgradeableFacilities.GetFacilityLevelCount(facility);
+            return level >= minLevel && level <= maxLevel;
         }
     }
 }
