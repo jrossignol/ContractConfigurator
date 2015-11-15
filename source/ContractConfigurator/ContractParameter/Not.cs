c6bb40b6ba62038175c23306b6cd5cdbb74aae5a
Jeremie Rossignol <jrossignol@gmail.com>
2015-11-14 19:37:10 -0700

    [#339] New Not parameter.

---
+++ source/ContractConfigurator/ContractParameter/Not.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+
+namespace ContractConfigurator.Parameters
+{
+    /// <summary>
+    /// ContractParameter that inverts the state of its child parameter.
+    /// </summary>
+    public class Not : ContractConfiguratorParameter
+    {
+        public Not()
+            : base(null)
+        {
+        }
+
+        public Not(string title)
+            : base(title)
+        {
+            disableOnStateChange = true;
+            state = ParameterState.Complete;
+        }
+
+        protected override string GetParameterTitle()
+        {
+            string output = null;
+            if (string.IsNullOrEmpty(title))
+            {
+                output = "Is not true";
+            }
+            else
+            {
+                output = title;
+            }
+
+            return output;
+        }
+
+        protected override void OnParameterSave(ConfigNode node)
+        {
+        }
+
+        protected override void OnParameterLoad(ConfigNode node)
+        {
+        }
+
+        protected override void OnRegister()
+        {
+            GameEvents.Contract.onParameterChange.Add(new EventData<Contract, ContractParameter>.OnEvent(OnAnyContractParameterChange));
+            ContractConfigurator.OnParameterChange.Add(new EventData<Contract, ContractParameter>.OnEvent(OnAnyContractParameterChange));
+        }
+
+        protected override void OnUnregister()
+        {
+            GameEvents.Contract.onParameterChange.Remove(new EventData<Contract, ContractParameter>.OnEvent(OnAnyContractParameterChange));
+            ContractConfigurator.OnParameterChange.Remove(new EventData<Contract, ContractParameter>.OnEvent(OnAnyContractParameterChange));
+        }
+
+        protected void OnAnyContractParameterChange(Contract contract, ContractParameter contractParameter)
+        {
+            if (contract == Root)
+            {
+                LoggingUtil.LogVerbose(this, "OnAnyContractParameterChange");
+                if (this.GetChildren().Where(p => p.State == ParameterState.Complete).Any())
+                {
+                    SetState(ParameterState.Incomplete);
+                }
+                else
+                {
+                    SetState(ParameterState.Complete);
+                }
+            }
+        }
+    }
+}
---
+++ source/ContractConfigurator/ParameterFactory/NotFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using Contracts;
+using Contracts.Parameters;
+using ContractConfigurator.Parameters;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ParameterFactory wrapper for Not ContractParameter.
+    /// </summary>
+    public class NotFactory : ParameterFactory
+    {
+        public override ContractParameter Generate(Contract contract)
+        {
+            return new Not(title);
+        }
+    }
+}
