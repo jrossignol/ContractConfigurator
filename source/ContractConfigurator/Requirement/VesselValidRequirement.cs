f2f461b9c4b5070f8717dead7cc561de64aa2231
Jeremie Rossignol <jrossignol@gmail.com>
2016-03-23 21:32:17 -0600

    Added new VesselValidRequirement - resolves #496.

---
+++ source/ContractConfigurator/Requirement/VesselValidRequirement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using KSP;
+using KSPAchievements;
+using Contracts;
+using ContractConfigurator.ExpressionParser;
+
+namespace ContractConfigurator
+{
+    /// <summary>
+    /// ContractRequirement to check if a VesselIdentifier is assigned to a valid vessel.
+    /// </summary>
+    public class VesselValidRequirement : ContractRequirement
+    {
+        protected string vessel;
+
+        public override bool Load(ConfigNode configNode)
+        {
+            // Load base class
+            bool valid = base.Load(configNode);
+
+            // Get expression
+            valid &= ConfigNodeUtil.ParseValue<string>(configNode, "vessel", x => vessel = x, this);
+
+            return valid;
+        }
+
+        public override bool RequirementMet(ConfiguredContract contract)
+        {
+            return ContractVesselTracker.Instance != null && ContractVesselTracker.Instance.GetAssociatedVessel(vessel) != null;
+        }
+    }
+}
