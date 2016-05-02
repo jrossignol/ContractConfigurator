bef4209c99d49d1a16409be79c8d18f5d92a55cb
Jeremie Rossignol <jrossignol@gmail.com>
2016-05-02 12:24:18 -0600

    Remove obscene debug.

--- source/ContractConfigurator/Requirement/PartModuleUnlockedRequirement.cs
+++ source/ContractConfigurator/Requirement/PartModuleUnlockedRequirement.cs
@@ -31,7 +31,6 @@ namespace ContractConfigurator
 
         public override void OnSave(ConfigNode configNode)
         {
-            Debug.Log("PartModuleUnlockedRequirement.OnSave");
             foreach (string pm in partModules)
             {
                 configNode.AddValue("partModule", pm);
@@ -40,39 +39,22 @@ namespace ContractConfigurator
 
         public override void OnLoad(ConfigNode configNode)
         {
-            Debug.Log("PartModuleUnlockedRequirement.OnLoad");
             partModules = ConfigNodeUtil.ParseValue<List<string>>(configNode, "partModule", new List<string>());
         }
 
         public override bool RequirementMet(ConfiguredContract contract)
         {
-            Debug.Log("PartModuleUnlockedRequirement.RequirementMet");
-            Debug.Log("    partModules = " + partModules);
             foreach (string partModule in partModules)
             {
-                Debug.Log("    partModule = " + partModule);
-                Debug.Log("    PartLoader.Instance = " + PartLoader.Instance);
-                Debug.Log("    PartLoader.Instance.parts = " + PartLoader.Instance.parts);
-
-                // Should never happen?
-                if (PartLoader.Instance == null || PartLoader.Instance.parts == null)
-                {
-                    return false;
-                }
-
 
                 // Search for a part that has our module
                 bool found = false;
                 foreach (AvailablePart p in PartLoader.Instance.parts)
                 {
-                    Debug.Log("        p = " + p);
-                    Debug.Log("        p.partPrefab = " + p.partPrefab);
-                    Debug.Log("        p.partPrefab.Modules = " + p.partPrefab.Modules);
                     if (p != null && p.partPrefab != null && p.partPrefab.Modules != null)
                     {
                         foreach (PartModule pm in p.partPrefab.Modules)
                         {
-                            Debug.Log("            pm = " + pm);
                             if (pm != null && pm.moduleName != null && pm.moduleName == partModule)
                             {
                                 found = true;
