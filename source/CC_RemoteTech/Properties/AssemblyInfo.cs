e6b8d090e04d4cff29dbf69d79e2b411f1031cdd
Jeremie Rossignol <jrossignol@gmail.com>
2016-05-26 22:36:42 -0600

    1.12.0

--- source/CC_RemoteTech/Properties/AssemblyInfo.cs
+++ source/CC_RemoteTech/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.11.5")]
-[assembly: AssemblyInformationalVersion("1.11.5")]
+[assembly: AssemblyFileVersion("1.12.0")]
+[assembly: AssemblyInformationalVersion("1.12.0")]
 [assembly: KSPAssembly("CC_RemoteTech", 1, 0)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
 [assembly: KSPAssemblyDependency("RemoteTech", 1, 7)]
