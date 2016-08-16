792426451e74d0064c5d30f1d04a3c032b75cf31
Jeremie Rossignol <jrossignol@gmail.com>
2016-08-15 20:52:09 -0600

    [#555] Added partModule and partModuleType.

--- source/ContractConfigurator/Requirement/TechResearchedRequirement.cs
+++ source/ContractConfigurator/Requirement/TechResearchedRequirement.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using KSP;
-using KSPAchievements;
+using FinePrint;
 
 namespace ContractConfigurator
 {
@@ -14,6 +14,8 @@ namespace ContractConfigurator
     public class TechResearchedRequirement : ContractRequirement
     {
         protected List<string> techs;
+        protected List<string> partModules;
+        protected List<string> partModuleTypes;
 
         public override bool LoadFromConfig(ConfigNode configNode)
         {
@@ -36,7 +38,10 @@ namespace ContractConfigurator
                 }
             }
 
-            valid &= ConfigNodeUtil.AtLeastOne(configNode, new string[] { "tech", "part" }, this);
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "partModule", x => partModules = x, this, new List<string>(), x => x.All(Validation.ValidatePartModule));
+            valid &= ConfigNodeUtil.ParseValue<List<string>>(configNode, "partModuleType", x => partModuleTypes = x, this, new List<string>(), x => x.All(Validation.ValidatePartModuleType));
+
+            valid &= ConfigNodeUtil.AtLeastOne(configNode, new string[] { "tech", "part", "partModule", "partModuleType" }, this);
 
             return valid;
         }
@@ -47,11 +52,23 @@ namespace ContractConfigurator
             {
                 configNode.AddValue("tech", tech);
             }
+
+            foreach (string partModule in partModules)
+            {
+                configNode.AddValue("partModule", partModule);
+            }
+
+            foreach (string partModuleType in partModuleTypes)
+            {
+                configNode.AddValue("partModuleType", partModuleType);
+            }
         }
 
         public override void OnLoad(ConfigNode configNode)
         {
             techs = ConfigNodeUtil.ParseValue<List<string>>(configNode, "tech", new List<string>());
+            partModules = ConfigNodeUtil.ParseValue<List<string>>(configNode, "partModule", new List<string>());
+            partModuleTypes = ConfigNodeUtil.ParseValue<List<string>>(configNode, "partModuleType", new List<string>());
         }
 
         public override bool RequirementMet(ConfiguredContract contract)
@@ -64,11 +81,69 @@ namespace ContractConfigurator
                     return false;
                 }
             }
+
+            foreach (string partModule in partModules)
+            {
+                bool hasModule = false;
+                foreach (AvailablePart part in PartLoader.LoadedPartsList)
+                {
+                    if (part.partPrefab == null || part.partPrefab.Modules == null)
+                    {
+                        continue;
+                    }
+
+                    if (ResearchAndDevelopment.PartTechAvailable(part))
+                    {
+                        hasModule = true;
+                        break;
+                    }
+                }
+
+                if (!hasModule)
+                {
+                    return false;
+                }
+            }
+
+            foreach (string partModuleType in partModuleTypes)
+            {
+                List<string> modules = ContractDefs.GetModules(partModuleType);
+
+                bool hasType = false;
+                foreach (string module in modules)
+                {
+                    foreach (AvailablePart part in PartLoader.LoadedPartsList)
+                    {
+                        if (part.partPrefab == null || part.partPrefab.Modules == null)
+                        {
+                            continue;
+                        }
+
+                        if (ResearchAndDevelopment.PartTechAvailable(part))
+                        {
+                            hasType = true;
+                            break;
+                        }
+                    }
+
+                    if (hasType)
+                    {
+                        break;
+                    }
+                }
+
+                if (!hasType)
+                {
+                    return false;
+                }
+            }
+
             return true;
         }
 
         protected override string RequirementText()
         {
+            // Techs
             string techStr = "";
             for (int i = 0; i < techs.Count; i++)
             {
@@ -86,8 +161,66 @@ namespace ContractConfigurator
 
                 techStr += Tech.GetTech(techs[i]).title;
             }
+            if (techs.Count > 0)
+            {
+                techStr = "have researched " + techStr;
+            }
 
-            return "Must " + (invertRequirement ? "not " : "") + "have researched " + techStr;
+            // Part module 
+            string pmStr = "";
+            for (int i = 0; i < partModules.Count; i++)
+            {
+                if (i != 0)
+                {
+                    if (i == partModules.Count - 1)
+                    {
+                        pmStr += " and ";
+                    }
+                    else
+                    {
+                        pmStr += ", ";
+                    }
+                }
+
+                pmStr += partModules[i];
+            }
+            if (partModules.Count > 0)
+            {
+                if (techs.Count > 0)
+                {
+                    techStr += " and";
+                }
+                pmStr = " have researched tech for " + pmStr;
+            }
+
+            // Part module type
+            string pmtStr = "";
+            for (int i = 0; i < partModuleTypes.Count; i++)
+            {
+                if (i != 0)
+                {
+                    if (i == partModuleTypes.Count - 1)
+                    {
+                        pmtStr += " and ";
+                    }
+                    else
+                    {
+                        pmtStr += ", ";
+                    }
+                }
+
+                pmtStr += partModuleTypes[i];
+            }
+            if (partModuleTypes.Count > 0)
+            {
+                if (techs.Count > 0 || partModules.Count > 0)
+                {
+                    pmStr += " and";
+                }
+                pmtStr = " have researched tech for " + pmtStr;
+            }
+
+            return "Must " + (invertRequirement ? "not " : "") + techStr + pmStr + pmtStr;
         }
     }
 }
