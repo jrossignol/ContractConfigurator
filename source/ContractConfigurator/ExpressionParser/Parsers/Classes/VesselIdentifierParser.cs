18f822dbbe35035bbbb06ac1fa01c17860ff12a6
Jeremie Rossignol <jrossignol@gmail.com>
2016-11-08 13:02:33 -0700

    Make vessel identifier parsing more greedy - fixes #599.

--- source/ContractConfigurator/ExpressionParser/Parsers/Classes/VesselIdentifierParser.cs
+++ source/ContractConfigurator/ExpressionParser/Parsers/Classes/VesselIdentifierParser.cs
@@ -61,11 +61,9 @@ namespace ContractConfigurator.ExpressionParser
 
         public override VesselIdentifier ParseIdentifier(Token token)
         {
-            // Try to parse more, as vessel names can have spaces
-            Match m = Regex.Match(expression, @"^((?>\s*[\w\d\-\.]+)+).*");
-            string identifier = m.Groups[1].Value;
-            expression = (expression.Length > identifier.Length ? expression.Substring(identifier.Length) : "");
-            identifier = token.sval + identifier;
+            // Vessel identifiers are greedy - grab the rest of the expression
+            string identifier = token.sval + expression;
+            expression = "";
 
             if (identifier.Equals("null", StringComparison.CurrentCultureIgnoreCase))
             {
