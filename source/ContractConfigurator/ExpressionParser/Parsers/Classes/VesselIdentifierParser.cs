509d88fb1cc3627f664f01c64cbb27f7bd7d7fea
Jeremie Rossignol <jrossignol@gmail.com>
2016-12-15 10:47:59 -0700

    Allow # character.

--- source/ContractConfigurator/ExpressionParser/Parsers/Classes/VesselIdentifierParser.cs
+++ source/ContractConfigurator/ExpressionParser/Parsers/Classes/VesselIdentifierParser.cs
@@ -62,7 +62,7 @@ namespace ContractConfigurator.ExpressionParser
         public override VesselIdentifier ParseIdentifier(Token token)
         {
             // Try to parse more, as vessel names can have spaces
-            Match m = Regex.Match(expression, @"^((?>\s*[\w\d\-\.]+)+).*");
+            Match m = Regex.Match(expression, @"^((?>\s*[\w\d\-\.#]+)+).*");
             string identifier = m.Groups[1].Value;
             expression = (expression.Length > identifier.Length ? expression.Substring(identifier.Length) : "");
             identifier = token.sval + identifier;
