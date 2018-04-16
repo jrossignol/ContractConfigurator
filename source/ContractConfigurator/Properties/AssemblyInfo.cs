218f10129dce3755c24598f0ad3c7be7e47ed079
Jeremie Rossignol <jrossignol@gmail.com>
2018-04-15 21:45:54 -0600

    1.25.0

--- source/ContractConfigurator/Properties/AssemblyInfo.cs
+++ source/ContractConfigurator/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.24.4")]
-[assembly: AssemblyInformationalVersion("1.24.4")]
+[assembly: AssemblyFileVersion("1.25.0")]
+[assembly: AssemblyInformationalVersion("1.25.0")]
 [assembly: KSPAssembly("ContractConfigurator", 1, 0)]
