ca3ca25071babd62ce9b6eb8016f75256a3218a9
Jeremie Rossignol <jrossignol@gmail.com>
2018-12-05 21:57:16 -0700

    1.26.0

--- source/KerKonConConExt/Properties/AssemblyInfo.cs
+++ source/KerKonConConExt/Properties/AssemblyInfo.cs
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.25.0")]
-[assembly: AssemblyInformationalVersion("1.25.0")]
+[assembly: AssemblyFileVersion("1.26.0")]
+[assembly: AssemblyInformationalVersion("1.26.0")]
 [assembly: KSPAssembly("KerKonConConExt", 1, 0)]
 [assembly: KSPAssemblyDependency("ContractConfigurator", 1, 0)]
 [assembly: KSPAssemblyDependency("KerbalKonstructs", 0, 9)]
