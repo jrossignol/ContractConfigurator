cf1cc88c9d0516694fe049e86823644679f18aba
Jeremie Rossignol <jrossignol@gmail.com>
2020-09-18 14:16:17 -0600

    Clean up Deployed Science spam.

---
+++ source/ContractConfigurator/ScenarioModules/ScienceReporter.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using KSP;
+using KSP.Localization;
+using KSP.UI.Screens;
+using Expansions;
+using ContractConfigurator.Util;
+
+namespace ContractConfigurator
+{
+    [KSPScenario(ScenarioCreationOptions.AddToExistingCareerGames | ScenarioCreationOptions.AddToNewCareerGames |
+        ScenarioCreationOptions.AddToExistingScienceSandboxGames | ScenarioCreationOptions.AddToNewScienceSandboxGames,
+        GameScenes.FLIGHT, GameScenes.TRACKSTATION, GameScenes.SPACECENTER)]
+    public class ScienceReporter : ScenarioModule
+    {
+        static FieldInfo messageListField = typeof(MessageSystem).GetFields(BindingFlags.Instance | BindingFlags.NonPublic).Where(fi => fi.FieldType == typeof(List<MessageSystemButton>)).First();
+        static List<MessageSystemButton> messageList = null;
+        static string deployedScienceTag = Localizer.GetStringByTag("#autoLOC_8002254");
+        static string deployedScienceMessageTitle = Localizer.GetStringByTag("#cc.science.deployedScienceReport");
+
+        int lastMessageCount = 0;
+        List<ScienceSubject> recentScience = new List<ScienceSubject>();
+        List<ScienceSubject> trackedSubjects = new List<ScienceSubject>();
+        List<ScreenMessage> screenMessagesToRemove = new List<ScreenMessage>();
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+
+            // Right now we only look at deployed science, which is a Breaking Ground feature
+            if (!ExpansionsLoader.IsExpansionInstalled("Serenity"))
+            {
+                Destroy(this);
+            }
+        }
+
+        void Start()
+        {
+            DontDestroyOnLoad(this);
+
+            GameEvents.onGUIMessageSystemReady.Add(new EventVoid.OnEvent(OnMessageSystemReady));
+            GameEvents.OnScienceRecieved.Add(new EventData<float, ScienceSubject, ProtoVessel, bool>.OnEvent(OnScienceReceived));
+        }
+
+        void Destroy()
+        {
+            GameEvents.onGUIMessageSystemReady.Remove(new EventVoid.OnEvent(OnMessageSystemReady));
+            GameEvents.OnScienceRecieved.Remove(new EventData<float, ScienceSubject, ProtoVessel, bool>.OnEvent(OnScienceReceived));
+        }
+
+        void Update()
+        {
+            // Detect if there are any new messages
+            if (MessageSystem.Instance != null)
+            {
+                // Derive the internal message list
+                if (messageList == null)
+                {
+                    messageList = (List<MessageSystemButton>)messageListField.GetValue(MessageSystem.Instance);
+                }
+
+                // Go through new messages
+                if (messageList.Count > lastMessageCount)
+                {
+                    for (int i = (messageList.Count - lastMessageCount); i-- > 0;)
+                    {
+                        LoggingUtil.LogVerbose(this, "Message list count = {0}, message {3} = {1} + {2}", messageList.Count, messageList[i].message.message, messageList[i].message.messageTitle, i);
+                        if (LocalizationUtil.IsLocalizedString(messageList[i].message.message, deployedScienceTag))
+                        {
+                            // Pull out the parameters
+                            IList<string> parameters = LocalizationUtil.UnLocalizeString(messageList[i].message.message, deployedScienceTag);
+
+                            // Identify the subject
+                            ScienceSubject subject = null;
+                            foreach (ScienceSubject sub in recentScience)
+                            {
+                                if (sub.title == parameters[0])
+                                {
+                                    subject = sub;
+                                    break;
+                                }
+                            }
+
+                            // Subject identified
+                            if (subject != null)
+                            {
+                                LoggingUtil.LogVerbose(this, "Subject identified as {0}", subject.id);
+
+                                // Delete the old message
+                                MessageSystem.Instance.DiscardMessage(messageList[i].message.button);
+
+                                // Check for an existing summary level message
+                                MessageSystem.Message message = MessageSystem.Instance.FindMessages(m => m.messageTitle == deployedScienceMessageTitle).FirstOrDefault();
+                                if (message != null)
+                                {
+                                    message.IsRead = false;
+                                    trackedSubjects.Clear();
+                                }
+                                trackedSubjects.Add(subject);
+                                trackedSubjects.Sort((ss1, ss2) => string.Compare(ss1.title, ss2.title));
+
+                                StringBuilder sb = StringBuilderCache.Acquire();
+                                sb.Append(string.Format("<b>{0}</b>:\n", deployedScienceMessageTitle));
+
+                                foreach (ScienceSubject s in trackedSubjects)
+                                {
+                                    sb.Append(string.Format("    {0}: <color=#6DCFF6><sprite=\"CurrencySpriteAsset\" name=\"Science\" tint=1> {1}</color> / <color=#6DCFF6><sprite=\"CurrencySpriteAsset\" name=\"Science\" tint=1> {2}</color>\n",
+                                        s.title, s.science.ToString("F1"), s.scienceCap.ToString("F1")));
+                                }
+
+                                if (message != null)
+                                {
+                                    message.message = sb.ToStringAndRelease();
+                                }
+                                else
+                                {
+                                    MessageSystem.Instance.AddMessage(new MessageSystem.Message(deployedScienceMessageTitle, sb.ToStringAndRelease(),
+                                        MessageSystemButton.MessageButtonColor.BLUE, MessageSystemButton.ButtonIcons.ALERT));
+                                }
+                            }
+                            else
+                            {
+                                LoggingUtil.LogWarning(this, "Couldn't identify subject for deployed experiment with title '{0}'", parameters[0]);
+                            }
+                        }
+                    }
+
+                    recentScience.Clear();
+                }
+                lastMessageCount = messageList.Count;
+
+                // Check for active screen messages
+                screenMessagesToRemove.Clear();
+                foreach (ScreenMessage message in ScreenMessages.Instance.ActiveMessages)
+                {
+                    if (LocalizationUtil.IsLocalizedString(message.message, deployedScienceTag))
+                    {
+                        screenMessagesToRemove.Add(message);
+                    }
+                }
+
+                // Remove the messages
+                foreach (ScreenMessage message in screenMessagesToRemove)
+                {
+                    ScreenMessages.RemoveMessage(message);
+                }
+            }
+        }
+
+        private void OnScienceReceived(float science, ScienceSubject subject, ProtoVessel protoVessel, bool reverseEngineered)
+        {
+            recentScience.Add(subject);
+        }
+
+        private void OnMessageSystemReady()
+        {
+            messageList = null;
+            lastMessageCount = 0;
+        }
+
+        public override void OnLoad(ConfigNode node)
+        {
+            try
+            {
+                base.OnLoad(node);
+
+                foreach (string id in node.GetValuesList("subject"))
+                {
+                    ScienceSubject s = ResearchAndDevelopment.GetSubjectByID(id);
+                    if (s != null)
+                    {
+                        trackedSubjects.Add(s);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.LogError(this, "Error loading ScienceReporter from persistance file!");
+                LoggingUtil.LogException(e);
+                ExceptionLogWindow.DisplayFatalException(ExceptionLogWindow.ExceptionSituation.SCENARIO_MODULE_LOAD, e, "ScienceReporter");
+            }
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            try
+            {
+                base.OnSave(node);
+
+                foreach (ScienceSubject s in trackedSubjects)
+                {
+                    node.AddValue("subject", s.id);
+                }
+            }
+            catch (Exception e)
+            {
+                LoggingUtil.LogError(this, "Error saving ScienceReporter to persistance file!");
+                LoggingUtil.LogException(e);
+                ExceptionLogWindow.DisplayFatalException(ExceptionLogWindow.ExceptionSituation.SCENARIO_MODULE_SAVE, e, "ScienceReporter");
+            }
+        }
+    }
+}
--- source/ContractConfigurator/Util/LocalizationUtil.cs
+++ source/ContractConfigurator/Util/LocalizationUtil.cs
@@ -36,7 +36,7 @@ namespace ContractConfigurator
         private LocalizationUtil()
         {
             andMiddle = Localizer.GetStringByTag("#cc.list.and.middle").Replace("<<1>>", "").Replace("<<2>>", "");
-            orMiddle  = Localizer.GetStringByTag("#cc.list.or.middle").Replace("<<1>>", "").Replace("<<2>>", "");
+            orMiddle = Localizer.GetStringByTag("#cc.list.or.middle").Replace("<<1>>", "").Replace("<<2>>", "");
             andEnd = Localizer.GetStringByTag("#cc.list.and.end").Replace("<<1>>", "").Replace("<<2>>", "");
             orEnd = Localizer.GetStringByTag("#cc.list.or.end").Replace("<<1>>", "").Replace("<<2>>", "");
         }
@@ -105,6 +105,92 @@ namespace ContractConfigurator
             ExperienceTraitConfig config = GameDatabase.Instance.ExperienceConfigs.Categories.Where(c => c.Name == traitName).FirstOrDefault();
 
             return config != null ? config.Title : traitName;
+        }
+
+        /// <summary>
+        /// Performs a very sketchy heuristic match on a localized string.  Will possibly give bad results on stuff like "<<1>> <<2>>".
+        /// </summary>
+        /// <param name="searchString">The string to search in.</param>
+        /// <param name="localizationTag">The localization tag.</param>
+        /// <returns>Whether the searchString is a parametized version of localizationTag.</returns>
+        public static bool IsLocalizedString(string searchString, string localizationTag)
+        {
+            LoggingUtil.LogVerbose(typeof(LocalizationUtil), "IsLocalizedString('{0}', '{1}')", searchString, localizationTag);
+
+            // First check for no parameter tags - literal comparison
+            int pos = localizationTag.IndexOf(">>");
+            if (pos == -1)
+            {
+                return string.Equals(searchString, localizationTag);
+            }
+            else
+            {
+                // Move past the >> characters
+                pos += 2;
+            }
+
+            // Find the second parameter
+            int pos2 = localizationTag.IndexOf("<<", pos);
+            if (pos2 == -1)
+            {
+                // No second parameter, just go to end of string
+                pos2 = localizationTag.Length;
+            }
+
+            string searchValue = localizationTag.Substring(pos, pos2 - pos);
+            bool match = (searchString.IndexOf(searchValue) > pos - 5);
+            LoggingUtil.LogVerbose(typeof(LocalizationUtil), "    the searchValue is '{0}', match = {1}", searchValue, match);
+
+            return match;
+        }
+
+        private static List<string> unlocalizedStringStorage = new List<string>();
+        /// <summary>
+        /// Reverses the localization of a string and gets the parameters out (as strings)
+        /// </summary>
+        /// <param name="input">Input string</param>
+        /// <param name="localizationTag">Localization tag to use to reverse localization.</param>
+        /// <returns></returns>
+        public static IList<string> UnLocalizeString(string input, string localizationTag)
+        {
+            LoggingUtil.LogVerbose(typeof(LocalizationUtil), "UnLocalizeString('{0}', '{1}')", input, localizationTag);
+            unlocalizedStringStorage.Clear();
+
+            int inputPos = 0;
+            int currentPos = 0;
+            int paramIndex = localizationTag.IndexOf("<<", currentPos);
+            int len = localizationTag.Length;
+            while (paramIndex >= 0 && paramIndex < len)
+            {
+                // Fast forward the input to the same location.
+                inputPos += paramIndex - currentPos;
+
+                //<<1>> <<2>>
+                // Get the location of the *next* parameter
+                currentPos = paramIndex + 5;
+                paramIndex = localizationTag.IndexOf("<<", currentPos);
+                if (paramIndex == -1)
+                {
+                    paramIndex = len;
+                }
+
+                // Get the next literal part of the localization tag
+                string searchValue = localizationTag.Substring(currentPos, paramIndex - currentPos);
+                int index = (currentPos == paramIndex) ? input.Length : input.IndexOf(searchValue, inputPos);
+                if (index == -1)
+                {
+                    LoggingUtil.LogError(typeof(LocalizationUtil), "Couldn't unlocalize string '{0}'", input);
+                    return unlocalizedStringStorage.AsReadOnly();
+                }
+
+                // Get the parameter and fast forward past it
+                string parameterValue = input.Substring(inputPos, index - inputPos);
+                inputPos = index;
+                LoggingUtil.LogVerbose(typeof(LocalizationUtil), "   adding parameter '{0}'", parameterValue);
+                unlocalizedStringStorage.Add(parameterValue);
+            }
+
+            return unlocalizedStringStorage.AsReadOnly();
         }
     }
 }
